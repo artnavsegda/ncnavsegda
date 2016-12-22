f706849e19ec732965086d9356f454af220dfe92
Art Navsegda <artnavsegda@gmail.com>
2016-12-22 08:22:37 +0300

    hello

--- src/hello.c
+++ src/hello.c
@@ -1,15 +1,29 @@
 #include <ncurses.h>
+#include <stdlib.h>
 
 int main(void)
 {
 	initscr();
 	noecho();
 	curs_set(FALSE);
+	atexit(endwin);
 
 	mvprintw(0,0, "Hello World");
 	refresh();
 
-	getch();
+	while(1)
+	{
+		switch (getch())
+		{
+			case 'q':
+				exit(0);
+			break;	
+			default:
+				refresh();
+			break;
+		}
+	}
+
 	endwin();
 
 	return 0;
